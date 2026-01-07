a937153148caea02cb2aca8a4f46084c4e550166
plastic041 <plastic041@gmail.com>
2026-01-07 10:03:44 +0900

    simpler

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,5 +6,8 @@ const note = useStorage("note", "");
 </script>
 
 <template>
-  <Editor v-model:model-value="note" />
+  <textarea
+    v-model="note"
+    class="h-dvh w-screen border-none outline-none m-0 fixed p-2 resize-none"
+  />
 </template>
